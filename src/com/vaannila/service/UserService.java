14aa036307f29391c1726905f58e9bd07400d45d
jerry <jerry@example.com>
2019-04-28 10:01:16 +0000

    Adde Jerry comment in UserService File

--- src/com/vaannila/service/UserService.java
+++ src/com/vaannila/service/UserService.java
@@ -5,5 +5,6 @@ import com.vaannila.domain.User;
 public interface UserService {
 
 	public void add(User user);
-        println("Hello Narenda")
+        println("Hello Narenda");
+	println("Hello Jerry");
 }
